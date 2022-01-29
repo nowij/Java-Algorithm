fcb364f3925c66d4524e38254e5b5e1a3072a5c7
ez <89061199+nowij@users.noreply.github.com>
2022-01-29 17:28:09 +0900

    Add files via upload

---
+++ 6. 재귀/No10870.java
@@ -0,0 +1,10 @@
+package baekjoon.recursion;
+
+public class No10870 {
+    public static int fibonacci(int num) {
+        if (num <= 0) return 0;
+        else if (num == 1) return 1;
+
+        return fibonacci(num-1)+fibonacci(num-2);
+    }
+}
