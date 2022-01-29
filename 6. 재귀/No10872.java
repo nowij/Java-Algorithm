f8bfc9c45397d3a40012cd9745aa33a91ebb7399
ez <89061199+nowij@users.noreply.github.com>
2022-01-29 16:50:38 +0900

    Update No10872.java

--- 6. 재귀/No10872.java
+++ 6. 재귀/No10872.java
@@ -2,18 +2,18 @@ package baekjoon.recursion;
 
 import java.io.*;
 
-public class No10872 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //입력
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); //출력
 
-        int number = Integer.parseInt(br.readLine()); // readLine은 String으로 한 줄씩 읽음
+        int number = Integer.parseInt(br.readLine());
         String answer = String.valueOf(recursive(number));
 
-        bw.write(answer); //버퍼에 입력 (int 안 들어감)
+        bw.write(answer); 
 
-        bw.flush(); // 버퍼의 내용 출력
-        bw.close(); // 버퍼 닫기
+        bw.flush();
+        bw.close();
     }
 
     public static int recursive(int num) {
