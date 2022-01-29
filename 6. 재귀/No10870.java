335cbc2c42e58e21494a5b557fd56b6a7b2caebd
ez <89061199+nowij@users.noreply.github.com>
2022-01-29 17:29:41 +0900

    백준 10870번 (피보나치 수 5)

--- 6. 재귀/No10870.java
+++ 6. 재귀/No10870.java
@@ -1,10 +1,23 @@
-package baekjoon.recursion;
+import java.io.*;
 
-public class No10870 {
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int number = Integer.parseInt(br.readLine());
+        String answer = String.valueOf(fibonacci(number));
+        
+        bw.write(answer);
+        bw.flush();
+        bw.close();
+        
+    }
+    
     public static int fibonacci(int num) {
         if (num <= 0) return 0;
-        else if (num == 1) return 1;
-
+        else if(num == 1) return 1;
+        
         return fibonacci(num-1)+fibonacci(num-2);
     }
 }
