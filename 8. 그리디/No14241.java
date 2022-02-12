8e3f9bdf208b7ef8eeb90b707caa17c18ad4cd4d
nowij <nowijdl@gmail.com>
2022-02-12 23:21:29 +0900

    백준 14241 (슬라임 합치기)

---
+++ 8. 그리디/No14241.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        br.readLine();
+        List<Integer> size = new ArrayList<Integer>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while(st.countTokens()!=0)
+            size.add(Integer.parseInt(st.nextToken()));
+
+        //오름차순으로 정렬
+        Collections.sort(size);
+        bw.write(String.valueOf(getScore(size,0,0)));
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static int getScore(List<Integer> list, int index, int score) {
+        if (index == list.size()-2)
+            return score + list.get(index) * list.get(index+1);
+        else {
+            score += list.get(index) * list.get(index+1);
+            list.set(index+1, list.get(index) + list.get(index+1));
+            return getScore(list, index+1, score);
+        }
+    }
+}
